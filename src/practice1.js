c57d4d8f929b41b3720e28ce75f53e7790a56118
xianhaoni <xianhaoni@gmail.com>
2020-06-14 12:22:37 +0800

    finsih the implementation for p1 and p2

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,16 @@
-const parseData
+const parseData = (input) => {
+    const {data,column} = input;
+    const columnItems = column.map(item => item.name);
+    const newData = new Array();
+
+        for(let key of data.keys()){
+            const newPerson = new Object();
+            for(let index of columnItems.keys()){
+                newPerson[columnItems[index]] = data[key][index];
+            }
+            newData.push(newPerson);
+        }
+    return newData;
+    
+}
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,15 @@
-const inject
+const inject = (arrayData, objectData) =>{
+
+    let currentPosition = 0;
+    const newArray = new Array();
+    for(let key of arrayData.keys()){
+        if(currentPosition<objectData.length)
+            if(key===objectData[currentPosition].index){
+                newArray.push(objectData[currentPosition].content);
+                currentPosition++;
+            }
+        newArray.push(arrayData[key]);    
+    }
+    return newArray;
+}
 export { inject };
